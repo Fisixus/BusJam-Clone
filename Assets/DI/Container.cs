e9e17a94f4769a27e92acdf44cc61e7c0806c7d1
Utku ERDEN <31778258+Fisixus@users.noreply.github.com>
2025-01-24 22:19:43 +0300

    Setup has been completed

---
+++ Assets/DI/Container.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DI
+{
+    public class Container
+    {
+        private readonly Dictionary<Type, Func<object>> _bindings = new();
+
+        public void BindAsSingle<T>(Func<T> factory) where T : class
+        {
+            T instance = null;
+            _bindings[typeof(T)] = () => instance ??= factory();
+        }
+
+        public void BindAsSingleNonLazy<T>(Func<T> factory) where T : class
+        {
+            BindAsSingle(factory);
+            _bindings[typeof(T)].Invoke(); // Force creation now (non-lazy)
+        }
+
+        public void BindAsTransient<T>(Func<T> factory) where T : class
+        {
+            _bindings[typeof(T)] = () => factory();
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            return Resolve(typeof(T)) as T;
+        }
+
+        public object Resolve(Type type)
+        {
+            if (_bindings.TryGetValue(type, out var factory))
+            {
+                var instance = factory();
+                InitializeIfPreInitializable(instance);
+                return instance;
+            }
+
+            throw new Exception($"Type {type.Name} not bound in container.");
+        }
+
+        private void InitializeIfPreInitializable(object instance)
+        {
+            if (instance is IPreInitializable preInit)
+            {
+                preInit.PreInitialize();
+            }
+        }
+    }
+}
