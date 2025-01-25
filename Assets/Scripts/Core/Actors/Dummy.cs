fd95448f411cfb69ff5bd670530c57ee407320ed
Utku ERDEN <31778258+Fisixus@users.noreply.github.com>
2025-01-25 22:34:27 +0300

    Outline effect has been imported

--- Assets/Scripts/Core/Actors/Dummy.cs
+++ Assets/Scripts/Core/Actors/Dummy.cs
@@ -8,6 +8,8 @@ namespace Core.Actors
         [field: SerializeField] public Animator Animator { get; private set; }
         [field: SerializeField] public Renderer JointRenderer { get; private set; }
         [field: SerializeField] public Renderer SurfaceRenderer { get; private set; }
+        [field: SerializeField] public Outline JointOutline { get; private set; }
+        [field: SerializeField] public Outline SurfaceOutline { get; private set; }
         [field: SerializeField] public Vector2Int Coordinate { get; set; }
         [field: SerializeField] public ColorType ColorType { get; set; }
         
@@ -28,6 +30,20 @@ namespace Core.Actors
         {
             JointRenderer.material.color = colorData.Colors[ColorType];
             SurfaceRenderer.material.color = colorData.Colors[ColorType];
+        }
+        
+        public void SetOutline(bool isOn)
+        {
+            if (isOn)
+            {
+                JointOutline.OutlineWidth = 6;
+                SurfaceOutline.OutlineWidth = 6;
+            }
+            else
+            {
+                JointOutline.OutlineWidth = 0;
+                SurfaceOutline.OutlineWidth = 0; 
+            }
         }
         
         public override string ToString()
